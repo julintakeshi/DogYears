8f02c6200a1b7ba865af2c453a57715ea8577043
julintakeshi <145675644+julintakeshi@users.noreply.github.com>
2023-12-10 21:20:04 -0300

    Add files via upload

---
+++ Dog Years/jotinha.js
@@ -0,0 +1,29 @@
+// 1
+let myAge = 25; // Esta variável armazena a minha idade em anos humanos.
+
+// 2
+let earlyYears = 2; // Estes são os dois primeiros anos de um cão, que contam mais do que os anos subsequentes.
+
+// 3
+earlyYears *= 10.5; // Cada um dos dois primeiros anos humanos representa 10,5 "anos de cão".
+
+// 4
+let laterYears = myAge - 2; // laterYears representa os anos restantes após os dois primeiros anos de um cão.
+
+// 5
+laterYears *= 4; // Cada ano humano adicional representa 4 "anos de cão".
+
+// 6
+console.log(earlyYears, laterYears); // Verifique se os valores correspondem às expectativas.
+
+// 7
+let myAgeInDogYears = earlyYears + laterYears; // Esta variável representa a minha idade em "anos de cão".
+
+// 8
+let myName = "SeuNome".toLowerCase(); // Esta linha de código está convertendo seu nome para letras minúsculas.
+
+// 9
+console.log(Meu nome é ${myName}. Tenho ${myAge} anos em anos humanos, que são ${myAgeInDogYears} anos em anos de cão.); // Este comando imprime a frase no console.
+
+// 10
+// Parabéns pelo seu trabalho! Agora você pode converter qualquer idade humana em "anos de cão". Tente alterar myAge para ver o que acontece. Se quiser um desafio extra, tente refazer este projeto sem o uso do operador *=.
